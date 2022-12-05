b77000c4646679bbb21c7cfbcd65cd79afd8bc44
Aristides Staffieri <aristides@aristidesstaffieri.com>
2022-12-05 14:39:54 -0700

    refactor day 1

--- day1/index.js
+++ day1/index.js
@@ -4,19 +4,17 @@ const path = require('path')
 async function findElfWithMostCalories() {
   try {
     const data = await fs.readFile(path.join(__dirname, '/input.txt'), { encoding: 'utf8' })
-    const calories = data.split('\n')
-    const elfToTotalCalories = calories.reduce((prev, calorie) => {
-      if (calorie !== '') {
-        prev.counter += Number(calorie)
-      } else {
-        prev.out.push(prev.counter)
-        prev.counter = 0
-      }
-      return prev
-    }, { counter: 0, out: [] })
-    const sorted = elfToTotalCalories.out.sort((a, b) => a - b)
-    const top3 = sorted.slice(-3)
-    return top3.reduce((prev, curr) => prev + curr)
+    const calories = data.split('\n\n')
+
+    return calories
+      .map(caloriesStr => {
+        const calorieSet = caloriesStr.split('\n')
+        return calorieSet.reduce((prev, curr) => prev + Number(curr), 0)
+      })
+      .sort((a, b) => a - b)
+      .slice(-3)
+      .reduce((prev, curr) => prev + curr)
+
   } catch (err) {
     console.log(err)
   }
