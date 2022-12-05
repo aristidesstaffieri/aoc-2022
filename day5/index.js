3e0b213510f4dea9380cf8e2291940375e21187d
Aristides Staffieri <aristides@aristidesstaffieri.com>
2022-12-05 09:35:00 -0700

    adds part 2 answer

--- day5/index.js
+++ day5/index.js
@@ -3,15 +3,25 @@ const path = require('path')
 
 const inputFile = path.join(__dirname, '/input.txt')
 
-function moveStackItem(fromStack, toStack) {
-  const [item] = fromStack.splice(0, 1)
-  return { fromStack, toStack: [item, ...toStack] }
+function moveSubStacks(fromStack, toStack, count) {
+  const items = fromStack.splice(0, count)
+  return { fromStack, toStack: [...items, ...toStack] }
 }
 
-async function part1() {
+function stackColsToStr(stackColumns) {
+  return Object.keys(stackColumns)
+    .map(col => {
+      const [letter] = stackColumns[col][0].match(/[a-zA-Z]+/)
+      return letter
+    })
+    .reduce((prev, curr) => prev + curr)
+}
+
+async function getColumnsAndInstructions() {
   const COL_WIDTH = 3
   const COL_SPACE = 1
   const COLUMN_COUNT = 9
+
   try {
     const data = await fs.readFile(inputFile, { encoding: 'utf8' })
     const [stacks, procedures] = data.split('\n\n')
@@ -39,25 +49,46 @@ async function part1() {
       return [match[1], match[2], match[3]]
     })
 
+    return { stackColumns, instructions }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function part1() {
+  try {
+    const { stackColumns, instructions } = await getColumnsAndInstructions()
+
     for (const instruction of instructions) {
       const [moveCount, from, to] = instruction
 
       for (let index = 1; index <= Number(moveCount); index++) {
-        const newStacks = moveStackItem(stackColumns[from.toString()], stackColumns[to.toString()])
+        const newStacks = moveSubStacks(stackColumns[from.toString()], stackColumns[to.toString()], 1)
         stackColumns[from.toString()] = newStacks.fromStack
         stackColumns[to.toString()] = newStacks.toStack 
       }
     }
 
-    const codes = Object.keys(stackColumns)
-      .map(col => {
-        const [letter] = stackColumns[col][0].match(/[a-zA-Z]+/)
-        return letter
-      })
-      .reduce((prev, curr) => prev + curr)
+    return stackColsToStr(stackColumns)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-    return codes
+async function part2() {
+  try {
+    const { stackColumns, instructions } = await getColumnsAndInstructions()
 
+    for (const instruction of instructions) {
+      const [moveCount, from, to] = instruction
+      const _moveCount = Number(moveCount)
+
+      const newStacks = moveSubStacks(stackColumns[from.toString()], stackColumns[to.toString()], _moveCount)
+      stackColumns[from.toString()] = newStacks.fromStack
+      stackColumns[to.toString()] = newStacks.toStack 
+    }
+
+    return stackColsToStr(stackColumns)
   } catch (error) {
     console.error(error)
   }
@@ -67,12 +98,13 @@ async function main() {
   const part1Answer = await part1()
   console.log(`part 1: ${part1Answer}`)
 
-  // const part2Answer = await part2()
-  // console.log(`part 2: ${part2Answer}`)
+  const part2Answer = await part2()
+  console.log(`part 2: ${part2Answer}`)
 }
 
 main()
 
 module.exports = {
-  part1
+  part1,
+  part2
 }
